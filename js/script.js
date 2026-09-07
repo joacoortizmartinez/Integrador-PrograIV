cf270ceb408c931d55faff1b60344c414eb072d1
Joaquin Ortiz M <joacoortizmartinez@gmail.com>
2026-09-07 14:24:11 -0300

    se agrego favoritos

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 const catalogo = document.getElementById("catalogo");
 const listaLibros = document.getElementById("lista-libros");
 const formularioAgregar = document.getElementById("form-agregar-libro");
+const genero = document.getElementById("genero-buscar");
 const buscador = document.getElementById("titulo-buscar");
 let ultimoId = 0;
 
 function crearTarjeta(libro){
-    return `<article id="tarjeta-libro">
+    return `<article class="tarjeta-libro ${libro.favorito ? "fav" : ""}">
                 <h3> ${libro.titulo} </h3>
                 <p> Id: ${libro.id}</p>
                 <p> Autor: ${libro.autor}</p>
                 <p> Genero: ${libro.genero}</p>
                 <p> Año: ${libro.anio}</p>
                 <label for="favorito"> Marcar como fav </label>
-                <input type="checkbox" name="favorito" id="favorito">
+                <input type="checkbox" class="checkbox-favorito">
             </article>
             `;
 } 
@@ -23,10 +24,25 @@ function mostrarLibros(librosAMostrar) {
     librosAMostrar.forEach(function (libro) {
         listaLibros.innerHTML += crearTarjeta(libro);
         ultimoId = libro.id;
+        
     });
 }
-
 mostrarLibros(libros);
+
+function cargarGeneros(libros){
+    const generos = new Set();
+
+    libros.forEach(function(libro){
+        generos.add(libro.genero);
+    });
+
+    generos.forEach(function(g){
+            genero.innerHTML += `<option value="${g}">${g}</option>`;
+        });
+};
+
+cargarGeneros(libros);
+
 
 formularioAgregar.addEventListener("submit", function(evento){
     evento.preventDefault();
@@ -43,7 +59,7 @@ formularioAgregar.addEventListener("submit", function(evento){
         "autor": autor, 
         "genero": genero, 
         "anio": anio, 
-        disponible: true,
+        "disponible": true
     };
     libros.push(libroNuevo);
 
@@ -65,3 +81,16 @@ buscador.addEventListener("input", function(evento){
     mostrarLibros(librosFiltrados);
 });
 
+
+listaLibros.addEventListener("change", function(evento) {
+    if (evento.target.classList.contains("checkbox-favorito")) {
+        
+        const tarjeta = evento.target.parentElement;
+
+        if (evento.target.checked) {
+            tarjeta.classList.add("fav");
+        } else {
+            tarjeta.classList.remove("fav");
+        }
+    }
+});
